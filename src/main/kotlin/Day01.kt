2a3c305577e657aef10e65fbce5935660fbfaf2a
joren <joren.kennes@kunlabora.be>
2023-12-02 14:18:06 +0100

    day 01

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,54 +1,58 @@
 fun main() {
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(Day01().part1(input))
+    println(Day01().part2(input))
 }
 
-fun part1(input: List<String>): Int {
-    return input.sumOf { combineFirstAndLastDigit(it) }
-}
+class Day01 {
 
-fun part2(input: List<String>): Int {
-    return input
-        .sumOf { combineFirstAndLastDigitWhenStringsCouldRepresentDigit(it) }
-}
-
-fun combineFirstAndLastDigit(stringValue: String): Int {
-    val firstDigit = stringValue.first { it.isDigit() }
-    val lastDigit = stringValue.last { it.isDigit() }
-    return "$firstDigit$lastDigit".toInt()
-}
-
-fun combineFirstAndLastDigitWhenStringsCouldRepresentDigit(stringValue: String): Int {
-    return "${findFirstDigit(stringValue)}${findLastDigit(stringValue)}".toInt()
-}
-
-fun findFirstDigit(stringValue: String): Char {
-    val indexOfFirstDigit = stringValue.indexOfFirst { it.isDigit() }.takeIf { it >= 0 } ?: stringValue.lastIndex
-    val sectionBeforeFirstDigit =  stringValue.substring(0, indexOfFirstDigit)
-    for (i in sectionBeforeFirstDigit.indices) {
-        val subSection = sectionBeforeFirstDigit.substring(0..i)
-        val matchingNumber = digits.entries.firstOrNull { digit -> subSection.contains(digit.key) }
-        if (matchingNumber != null) {
-            return matchingNumber.value
-        }
+    fun part1(input: List<String>): Int {
+        return input.sumOf { combineFirstAndLastDigit(it) }
     }
 
-    return stringValue[indexOfFirstDigit];
-}
-
-fun findLastDigit(stringValue: String): Char {
-    val indexOfLastDigit = stringValue.indexOfLast { it.isDigit() }.takeIf { it >= 0 } ?: 0;
-    val sectionAfterLastDigit =  stringValue.substring(indexOfLastDigit + 1)
-    for (i in sectionAfterLastDigit.lastIndex downTo 0) {
-        val subSection = sectionAfterLastDigit.substring(i)
-        val matchingNumber = digits.entries.firstOrNull { digit -> subSection.contains(digit.key) }
-        if (matchingNumber != null) {
-            return matchingNumber.value
-        }
+    fun part2(input: List<String>): Int {
+        return input
+            .sumOf { combineFirstAndLastDigitWhenStringsCouldRepresentDigit(it) }
     }
 
-    return stringValue[indexOfLastDigit];
+    fun combineFirstAndLastDigit(stringValue: String): Int {
+        val firstDigit = stringValue.first { it.isDigit() }
+        val lastDigit = stringValue.last { it.isDigit() }
+        return "$firstDigit$lastDigit".toInt()
+    }
+
+    fun combineFirstAndLastDigitWhenStringsCouldRepresentDigit(stringValue: String): Int {
+        return "${findFirstDigit(stringValue)}${findLastDigit(stringValue)}".toInt()
+    }
+
+    fun findFirstDigit(stringValue: String): Char {
+        val indexOfFirstDigit = stringValue.indexOfFirst { it.isDigit() }.takeIf { it >= 0 } ?: stringValue.lastIndex
+        val sectionBeforeFirstDigit = stringValue.substring(0, indexOfFirstDigit)
+        for (i in sectionBeforeFirstDigit.indices) {
+            val subSection = sectionBeforeFirstDigit.substring(0..i)
+            val matchingNumber = digits.entries.firstOrNull { digit -> subSection.contains(digit.key) }
+            if (matchingNumber != null) {
+                return matchingNumber.value
+            }
+        }
+
+        return stringValue[indexOfFirstDigit];
+    }
+
+    fun findLastDigit(stringValue: String): Char {
+        val indexOfLastDigit = stringValue.indexOfLast { it.isDigit() }.takeIf { it >= 0 } ?: 0;
+        val sectionAfterLastDigit = stringValue.substring(indexOfLastDigit + 1)
+        for (i in sectionAfterLastDigit.lastIndex downTo 0) {
+            val subSection = sectionAfterLastDigit.substring(i)
+            val matchingNumber = digits.entries.firstOrNull { digit -> subSection.contains(digit.key) }
+            if (matchingNumber != null) {
+                return matchingNumber.value
+            }
+        }
+
+        return stringValue[indexOfLastDigit];
+    }
+
 }
 
 val digits = mapOf(
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -7,13 +7,13 @@ class Day01Test {
 
     @Test
     fun `Part1 test example`() {
-        assertThat(part1(listOf("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"))).isEqualTo(142);
+        assertThat(Day01().part1(listOf("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"))).isEqualTo(142);
     }
 
     @Test
     fun `Part 2 test example`() {
         assertThat(
-            part2(
+            Day01().part2(
                 listOf(
                     "two1nine",
                     "eightwothree",
@@ -34,32 +34,32 @@ class Day01Test {
 
         @Test
         fun `When first and last character is digit Then combine to 2-digit number`() {
-            assertThat(combineFirstAndLastDigit("1abc2")).isEqualTo(12)
+            assertThat(Day01().combineFirstAndLastDigit("1abc2")).isEqualTo(12)
         }
 
         @Test
         fun `When digits are nested into other characters`() {
-            assertThat(combineFirstAndLastDigit("pqr3stu8vwx")).isEqualTo(38)
+            assertThat(Day01().combineFirstAndLastDigit("pqr3stu8vwx")).isEqualTo(38)
         }
 
         @Test
         fun `When multiple digits are nested into other characters`() {
-            assertThat(combineFirstAndLastDigit("a1b2c3d4e5f")).isEqualTo(15)
+            assertThat(Day01().combineFirstAndLastDigit("a1b2c3d4e5f")).isEqualTo(15)
         }
 
         @Test
         fun `When only one digit exists`() {
-            assertThat(combineFirstAndLastDigit("treb7uchet")).isEqualTo(77)
+            assertThat(Day01().combineFirstAndLastDigit("treb7uchet")).isEqualTo(77)
         }
 
         @Test
         fun `When only 2 digits`() {
-            assertThat(combineFirstAndLastDigit("94")).isEqualTo(94)
+            assertThat(Day01().combineFirstAndLastDigit("94")).isEqualTo(94)
         }
 
         @Test
         fun `When only no other characters`() {
-            assertThat(combineFirstAndLastDigit("92374")).isEqualTo(94)
+            assertThat(Day01().combineFirstAndLastDigit("92374")).isEqualTo(94)
         }
     }
 
@@ -69,33 +69,33 @@ class Day01Test {
 
         @Test
         fun `When one real digit and one spelled out before and after that digit `() {
-            assertThat(combineFirstAndLastDigitWhenStringsCouldRepresentDigit("two1nine")).isEqualTo(29);
+            assertThat(Day01().combineFirstAndLastDigitWhenStringsCouldRepresentDigit("two1nine")).isEqualTo(29);
         }
 
         @Test
         fun `When no real digits`() {
-            assertThat(combineFirstAndLastDigitWhenStringsCouldRepresentDigit("eightwothree")).isEqualTo(83);
+            assertThat(Day01().combineFirstAndLastDigitWhenStringsCouldRepresentDigit("eightwothree")).isEqualTo(83);
         }
 
         @Test
         fun `When spelledOut digit combined with other letters`() {
-            assertThat(combineFirstAndLastDigitWhenStringsCouldRepresentDigit("abcone2threexyz")).isEqualTo(13);
-            assertThat(combineFirstAndLastDigitWhenStringsCouldRepresentDigit("xtwone3four")).isEqualTo(24);
+            assertThat(Day01().combineFirstAndLastDigitWhenStringsCouldRepresentDigit("abcone2threexyz")).isEqualTo(13);
+            assertThat(Day01().combineFirstAndLastDigitWhenStringsCouldRepresentDigit("xtwone3four")).isEqualTo(24);
         }
 
         @Test
         fun `When spelled out digits but first and last are regular digits`() {
-            assertThat(combineFirstAndLastDigitWhenStringsCouldRepresentDigit("4nineeightseven2")).isEqualTo(42);
+            assertThat(Day01().combineFirstAndLastDigitWhenStringsCouldRepresentDigit("4nineeightseven2")).isEqualTo(42);
         }
 
         @Test
         fun `When first digit is spelled out en last one is regular digit`() {
-            assertThat(combineFirstAndLastDigitWhenStringsCouldRepresentDigit("zoneight234")).isEqualTo(14);
+            assertThat(Day01().combineFirstAndLastDigitWhenStringsCouldRepresentDigit("zoneight234")).isEqualTo(14);
         }
 
         @Test
         fun `When first digit is regular and last one is spelled out`() {
-            assertThat(combineFirstAndLastDigitWhenStringsCouldRepresentDigit("7pqrstsixteen")).isEqualTo(76);
+            assertThat(Day01().combineFirstAndLastDigitWhenStringsCouldRepresentDigit("7pqrstsixteen")).isEqualTo(76);
         }
     }
 }
